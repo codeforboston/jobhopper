b9dfb359792cbc41ceb7f268474f923f5a99f961
Endy Iskandar Imam <imam.endy@gmail.com>
2020-08-11 20:23:22 -0400

    Create barebone jobhopper app and view.

---
+++ src/jobhopper/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
--- src/urls.py
+++ src/urls.py
@@ -16,6 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+import src.jobhopper.views as views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # ex: /polls/5/
+    path('<int:question_id>/', views.detail, name='detail'),
+    # ex: /polls/5/results/
+    path('<int:question_id>/results/', views.results, name='results'),
+    # ex: /polls/5/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
