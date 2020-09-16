378a1874138cff87ac649db9dc04af204a8b4be8
pgibb96 <pgibb96@gmail.com>
2020-09-15 20:31:50 -0400

    Finally fixed .env SECRET_KEY not loading

--- jobhopper/settings.py
+++ jobhopper/settings.py
@@ -21,6 +21,8 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 import os
+from dotenv import load_dotenv
+load_dotenv()
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
