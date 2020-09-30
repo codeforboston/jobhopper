b24bd3d088f4793415b678f9a6efe27018a5cb94
Sean Greene <seangreenebrandeis@gmail.com>
2020-09-29 20:06:38 -0400

    added /state url for pulling back list of entries in DBT stateabbpairs

--- jobs/urls.py
+++ jobs/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path, include
 from . import views
 from rest_framework import routers
-from .api import LeadViewSet, BlsOesViewSet
+from .api import LeadViewSet, BlsOesViewSet, StateViewSet
 
 router = routers.DefaultRouter()
 router.register('leads', LeadViewSet)
 router.register('soc-codes', BlsOesViewSet)
+router.register(r'state', StateViewSet, basename='abbr')
+# router.register(r'state(?P<state_abb>.+)/$', StateViewSet, basename='state-name')
 
 urlpatterns = [
     path('', include(router.urls)),
+   
+
 ]
