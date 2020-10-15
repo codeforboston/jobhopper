bf7261303003e525ed031ba9f830f25dde57a084
jedpittman <jedpittman@gmail.com>
2020-10-15 19:42:06 -0400

    Update Treemap.tsx
    Add Alex B's Type fixes to the file

--- frontend/src/ui/D3Visualizations/Treemap.tsx
+++ frontend/src/ui/D3Visualizations/Treemap.tsx
@@ -1,58 +1,62 @@
-import React, { useLayoutEffect, useRef } from 'react';
+import React, { useCallback, useLayoutEffect, useRef } from 'react';
 import * as d3 from 'd3';
 import { TreeHierarchy } from '../../domain/hierarchy';
+import { Transition } from '../../domain/transition';
+
+const fontSize = 12;
+const white = '#ffffff';
+
+export type TreeNode = TreeHierarchy | Transition;
 
 export type TreemapProps = {
   height: number;
   width: number;
-  data: TreeHierarchy;
+  data: TreeNode;
 };
+
+function isTransition(node: TreeNode): node is Transition {
+  return (node as Transition).transitionRate !== undefined;
+}
+
+function transitionRate(node: TreeNode): number {
+  return isTransition(node) ? node.transitionRate : 0;
+}
 
 export default function Treemap({ height, width, data }: TreemapProps) {
   const containerRef = useRef<HTMLDivElement | null>(null);
 
-  useLayoutEffect(() => {
-    renderTreemap();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  function renderTreemap() {
-    const container: HTMLDivElement | null = containerRef.current;
+  const renderTreemap = useCallback(() => {
+    const container: HTMLDivElement = containerRef.current!;
 
     // create svg
-    const svg: d3.Selection<
-      SVGSVGElement,
-      unknown,
-      null,
-      undefined
-    > = d3
+    const svg = d3
       .select(container)
       .append('svg')
       .attr('width', width)
       .attr('height', height);
 
     // create hierarchical layout with data
-    const root: d3.HierarchyNode<TreeHierarchy> = d3
+    const root = d3
       .hierarchy(data)
-      .sum((d: any) => d.transitionRate)
-      .sort((a: any, b: any) => b.transitionRate - a.transitionRate);
+      .sum(transitionRate)
+      .sort((a, b) => a.value! - b.value!);
 
     // initialize treemap
-    const treemap = d3.treemap().size([width, height]).padding(1);
-
-    treemap(root);
+    const treemapRoot = d3.treemap<TreeNode>().size([width, height]).padding(1)(
+      root
+    );
 
     // select the nodes and set x, y position
     const nodes = svg
       .selectAll('g')
-      .data(root.leaves())
+      .data(treemapRoot.leaves())
       .enter()
       .append('g')
-      .attr('transform', (d: any) => `translate(${d.x0},${d.y0})`);
+      .attr('transform', d => `translate(${d.x0},${d.y0})`);
 
     // lighten colors
     const fader = (color: string): string =>
-      d3.interpolateRgb(color, '#fff')(0.3);
+      d3.interpolateRgb(color, white)(0.3);
 
     // select color within d3.schemeSet3 (contains array of 12 colors)
     const colorScale = d3.scaleOrdinal(d3.schemeSet3.map(fader));
@@ -60,25 +64,33 @@ export default function Treemap({ height, width, data }: TreemapProps) {
     // create and fill svg 'rects' based on the node data selected
     nodes
       .append('rect')
-      .attr('width', (d: any) => d.x1 - d.x0)
-      .attr('height', (d: any) => d.y1 - d.y0)
-      .attr('fill', (d: any) => colorScale(d.data.transitionRate));
+      .attr('width', d => d.x1 - d.x0)
+      .attr('height', d => d.y1 - d.y0)
+      .attr(
+        'fill',
+        d => colorScale(transitionRate(d.data).toString()) || white
+      );
 
     // add node labels
-    const fontSize: number = 12;
     nodes
       .append('text')
       .selectAll('tspan')
-      .data((d: any) =>
-        d.data.name.split(/(?=[A-Z][a-z])|\s+/g).concat(d.data.transitionRate)
+      .data(d =>
+        d.data.name
+          .split(/(?=[A-Z][a-z])|\s+/g)
+          .concat(transitionRate(d.data).toString())
       )
       .enter()
       .append('tspan')
       .attr('font-size', `${fontSize}px`)
       .attr('x', 3)
-      .attr('y', (_: any, i: number) => fontSize * i + fontSize)
-      .text((d: any) => d);
-  }
+      .attr('y', (_, i) => fontSize * i + fontSize)
+      .text(d => d);
+  }, [data, height, width]);
+
+  useLayoutEffect(() => {
+    renderTreemap();
+  }, [renderTreemap]);
 
   return <div ref={containerRef}></div>;
 }
