accdd00c89068f20e57e0fe89663d8a24eefbe57
Yuphonic <jyang@jades-mbp.lan>
2020-10-20 21:55:31 -0400

    serve swagger-ui api docs

--- jobhopper/settings.py
+++ jobhopper/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'rest_framework_swagger',
+    'drf_yasg',
     'jobs.apps.JobsConfig',
 
 
