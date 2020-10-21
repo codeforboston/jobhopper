5fc83242b5ec669fd4fd060d9540ceeb536224f4
Alex <alexjrussian@gmail.com>
2020-10-21 12:38:18 -0400

    Removed unnecessary hierarchy data files, updated Treemap story

--- frontend/src/ui/D3Visualizations/Treemap.stories.tsx
+++ frontend/src/ui/D3Visualizations/Treemap.stories.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Story, Meta } from '@storybook/react/types-6-0';
 import Treemap, { TreemapProps } from './Treemap';
-import data from '../../testing/data/hierarchy';
+import data from '../../testing/data/transitionData';
 
 export default {
   title: 'JobHopper/Treemap',
@@ -9,4 +9,4 @@ export default {
 } as Meta;
 
 export const Default: Story<TreemapProps> = args => <Treemap {...args} />;
-Default.args = { height: 400, width: 800, data };
+Default.args = { data };
