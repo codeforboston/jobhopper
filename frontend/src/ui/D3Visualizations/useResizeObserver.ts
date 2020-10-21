0ea0599bca49ab1b5530c3db8f2761a1bdac48b7
Alex <alexjrussian@gmail.com>
2020-10-21 12:37:11 -0400

    Created resize observer to watch for window resize

---
+++ frontend/src/ui/D3Visualizations/useResizeObserver.ts
@@ -0,0 +1,30 @@
+import { useEffect, useState } from 'react';
+import ResizeObserver from 'resize-observer-polyfill';
+
+interface Dimensions {
+  width: number;
+  height: number;
+}
+
+const useResizeObserver = (
+  ref: React.RefObject<HTMLDivElement>
+): Dimensions => {
+  const [dimensions, setDimensions] = useState({ width: 600, height: 300 });
+
+  useEffect(() => {
+    const observeTarget = ref.current!;
+    const resizeObserver = new ResizeObserver(entries => {
+      entries.forEach(entry => {
+        const { width, height } = entry.contentRect;
+        setDimensions({ width, height });
+      });
+    });
+    resizeObserver.observe(observeTarget);
+    return () => {
+      resizeObserver.unobserve(observeTarget);
+    };
+  }, [ref]);
+  return dimensions;
+};
+
+export default useResizeObserver;
