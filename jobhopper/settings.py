b0aef8864d95bda0f4cdc1e0a22fd275c4718b62
Lilian Cheung <35081231+l-cheung@users.noreply.github.com>
2020-10-30 21:35:48 -0400

    Update settings.py
    Add missing comma to identify throttle classes as a 1-element tuple & get server running

--- jobhopper/settings.py
+++ jobhopper/settings.py
@@ -53,7 +53,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 100,
     'DEFAULT_THROTTLE_CLASSES': (
-        'rest_framework.throttling.AnonRateThrottle'
+        'rest_framework.throttling.AnonRateThrottle',
     ),
     'DEFAULT_THROTTLE_RATES': {
         'anon': '300/day'
