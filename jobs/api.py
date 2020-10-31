538008d3f160a79b963319add32474ae981ee672
l-cheung <lilian.c.cheung@gmail.com>
2020-10-31 13:12:35 -0400

    Add filtering to occupation transitions and BLS OES API endpoints. Issue #160

--- jobs/api.py
+++ jobs/api.py
@@ -1,12 +1,14 @@
 from jobs.models import Socs, BlsOes, StateAbbPairs, OccupationTransitions
 from rest_framework import viewsets, permissions, generics
 from rest_framework.throttling import AnonRateThrottle
+import django_filters
 from .serializers import (
     SocSerializer,
     BlsOesSerializer,
     StateNamesSerializer,
     OccupationTransitionsSerializer,
 )
+
 
 # Lead Viewset
 class SocViewSet(viewsets.ModelViewSet):
@@ -16,11 +18,25 @@ class SocViewSet(viewsets.ModelViewSet):
     throttle_classes = [AnonRateThrottle]
 
 
+class BlsOesFilter(django_filters.FilterSet):
+    """
+    Create a filter to use with the BlsOes model. When multiple options are chosen in these filters, there
+    must be no space between comma-separated values
+    """
+    socs = django_filters.BaseInFilter(field_name='soc_code', lookup_expr='in')
+    areas = django_filters.BaseInFilter(field_name='area_title', lookup_expr='in')
+
+    class Meta:
+        model = BlsOes
+        fields = ['socs', 'areas']
+
+
 class BlsOesViewSet(viewsets.ModelViewSet):
     queryset = BlsOes.objects.all()
     permission_classes = [permissions.AllowAny]
     serializer_class = BlsOesSerializer
     throttle_classes = [AnonRateThrottle]
+    filter_class = BlsOesFilter
 
 
 class StateViewSet(viewsets.ModelViewSet):
@@ -30,9 +46,21 @@ class StateViewSet(viewsets.ModelViewSet):
     throttle_classes = [AnonRateThrottle]
 
 
+class OccupationTransitionsFilter(django_filters.FilterSet):
+    """
+    Create a filter to use with the OccupationTransitions model in the Occupation Transitions viewset
+    """
+    # field_name instead of name, and lookup_expr instead of lookup_type is used for the NumberFilter for Django 2.0+
+    min_transition_probability = django_filters.NumberFilter(field_name="pi", lookup_expr='gte')
+
+    class Meta:
+        model = OccupationTransitions
+        fields = ['min_transition_probability', 'soc1']
+
+
 class OccupationTransitionsViewSetFive(viewsets.ModelViewSet):
-    # Todo fix this using filtering instead of limiting the results
-    queryset = OccupationTransitions.objects.all()[:500]
+    queryset = OccupationTransitions.objects.all()
     permission_classes = [permissions.AllowAny]
     serializer_class = OccupationTransitionsSerializer
     throttle_classes = [AnonRateThrottle]
+    filter_class = OccupationTransitionsFilter
