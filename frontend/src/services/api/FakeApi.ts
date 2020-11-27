d8aec9ec94b51a7c52238ad337dc9d4adca29726
Alex Ball <me@alexjball.com>
2020-11-27 12:14:12 -0500

    Update API contract to match backend

--- frontend/src/services/api/FakeApi.ts
+++ frontend/src/services/api/FakeApi.ts
@@ -1,17 +1,22 @@
-import { Occupation } from '../../domain/occupation';
-import { State } from '../../domain/state';
-import { Transition } from '../../domain/transition';
 import occupations from '../../testing/data/occupations';
 import states from '../../testing/data/states';
 import transitions from '../../testing/data/transitionData';
-import Api from './Api';
+import Api, { GetTransitionRequest } from './Api';
+
+const resolveWithDelay = <T>(value: T): Promise<T> =>
+  new Promise(resolve => setTimeout(() => resolve(value), 500));
 
 export default class FakeApi implements Api {
-  getTransitions: () => Promise<Transition[]> = () =>
-    Promise.resolve(transitions);
-
-  getStates: () => Promise<State[]> = () => Promise.resolve(states);
-
-  getOccupations: () => Promise<Occupation[]> = () =>
-    Promise.resolve(occupations);
+  getTransitions = (request: GetTransitionRequest) => {
+    console.log('FakeApi.getTransitions', request);
+    return resolveWithDelay(transitions);
+  };
+  getStates = () => {
+    console.log('FakeApi.getStates');
+    return resolveWithDelay(states);
+  };
+  getOccupations = () => {
+    console.log('FakeApi.getOccupations');
+    return resolveWithDelay(occupations);
+  };
 }
