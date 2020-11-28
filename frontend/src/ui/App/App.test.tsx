ac4011817847020f4d4cddbbdc656e9c3947f2a9
Alex Ball <me@alexjball.com>
2020-11-27 21:36:13 -0500

    Wrap app in memory router

--- frontend/src/ui/App/App.test.tsx
+++ frontend/src/ui/App/App.test.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import { App } from './App';
+import { MemoryRouter } from 'react-router-dom';
 
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const { getByText } = render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>
+  );
+  const linkElement = getByText(/Occupation Transitions/i);
   expect(linkElement).toBeInTheDocument();
 });
