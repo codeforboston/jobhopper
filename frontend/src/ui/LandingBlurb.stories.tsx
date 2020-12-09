bcf59f1821e54b5f36d3e822079b338d6d94182a
Alex Ball <me@alexjball.com>
2020-12-08 21:33:01 -0500

    Combine landing and transition page.
    Make landing blurb collapsible.

--- frontend/src/ui/LandingBlurb.stories.tsx
+++ frontend/src/ui/LandingBlurb.stories.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 // also exported from '@storybook/react' if you can deal with breaking changes in 6.1
 import { Story, Meta } from '@storybook/react/types-6-0';
 
-import LandingPage from './LandingPage';
+import LandingBlurb from './LandingBlurb';
 
 export default {
-  title: 'JobHopper/LandingPage',
-  component: LandingPage,
+  title: 'JobHopper/LandingBlurb',
+  component: LandingBlurb,
 } as Meta;
 
-const Template: Story = () => <LandingPage />;
+const Template: Story = () => <LandingBlurb />;
 
 export const Default = Template.bind({});
