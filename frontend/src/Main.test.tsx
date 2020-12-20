112eca17bc05f5ea40304e60e7e87d118dd5d076
sashamaryl <sashamaryl@gmail.com>
2020-12-20 12:19:23 -0500

    ui tests beginning

--- frontend/src/Main.test.tsx
+++ frontend/src/Main.test.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
-import { render, wait } from '@testing-library/react';
+// import { render, wait } from '@testing-library/react';
 import Main from './Main';
 import { fireEvent, getQueriesForElement } from '@testing-library/dom';
-import { Input, Select } from '@material-ui/core';
+import { Input, /*Select*/ } from '@material-ui/core';
 import userEvent from '@testing-library/user-event';
+
+
+import Select from 'react-select'
+import { render, wait } from '@testing-library/react'
+import selectEvent from 'react-select-event'
+
 
 // renders the Main component, fills out the transition form, and renders the results table using the FakeApi. This tests interactions with the form and the container logic.
 
@@ -13,38 +19,41 @@ const { getByText, getByLabelText, getAllByTestId, findByText } = render(
   <Main />
 );
 
-const occupationInput = getByLabelText('Select occupation...');
-const stateInput = getByLabelText('Select state...');
+// const occupationInput = getByText('Select occupation...');
+// const stateInput = getByText('Select state...');
 
-test('renders learn react link', () => {
-  const { getByText } = render(<Main />);
-  const instructions = getByText(/enter occupation/i);
-  expect(instructions).toBeInTheDocument();
-});
+// test('renders learn react link', () => {
+//   const { getByText } = render(<Main />);
+//   const instructions = getByText(/enter occupation/i);
+//   expect(instructions).toBeInTheDocument();
+// });
 
-test('renders correct content from transitions page', async () => {
-  const { getByLabelText } = render(<Main />);
+// test('renders correct content from transitions page', async () => {
+//   const { getByText, getByDisplayValue, getByLabelText } = render(<Main />);
 
-  const occupationInput = getByLabelText('Select occupation...');
-  const stateInput = getByLabelText('Select state...');
 
-  expect(occupationInput).not.toBeNull();
-  expect(stateInput).not.toBeNull();
-});
+//   const occupationInput = getByDisplayValue('Select occupation');
+//   // const occupationInput = getByLabelText('Select occupation...');
+//   const stateInput = getByDisplayValue('Select state...');
+
+//   expect(occupationInput).not.toBeNull();
+//   expect(stateInput).not.toBeNull();
+// });
 
 test('allows user to select occupation and state from select menus', async () => {
-  const { getByLabelText } = render(<Main />);
+  const { getByText, getByLabelText: getByTestId } = render(<Main />);
 
+  await selectEvent.select(getByTestId('occupation-select'))
   userEvent.selectOptions(occupationInput, '01-2345 | Doctor');
   userEvent.selectOptions(stateInput, 'California');
 
   // fireEvent.change(occupationInput, {target: {value: 1}});
   // fireEvent.change(stateInput, { target: {value: 1} });
 
-  getByLabelText('See a Matrix').click(); //render table
+  getByText('See a Matrix').click(); //render table
 
   await wait(() => expect(findByText('Job Transitions')).not.toBeEmpty()); //check table renders content correctly
 
-  getByLabelText('See a Treechart').click();
+  getByText('See a Treechart').click();
   await wait(() => expect(findByText('Job Transitions')).not.toBeEmpty());
 });
