f95c8ce16cef63cd410d375b4752e378ec6690be
sashamaryl <sashamaryl@gmail.com>
2021-03-12 13:02:00 -0500

    adjusting spacing in the tool tip display

--- frontend/src/ui/D3Visualizations/ToolTip.tsx
+++ frontend/src/ui/D3Visualizations/ToolTip.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import OnetLink from '../OnetLink';
-import { SimpleFlexRow, TextContainer } from './styledDivs';
+import { SimpleFlexRow, SimpleFlexUnit, TextContainer } from './styledDivs';
 
 export default function ToolTipDisplay(props: any) {
   const { info } = props;
@@ -25,43 +25,33 @@ const ToolTipData = (props: any) => {
 
   return (
     <TextContainer>
-      <SimpleFlexRow>
-        <div style={{ marginRight: '2em' }}>{info.data.code}</div>
-        <div style={{ flex: 3 }}>
+      <SimpleFlexRow style={{ justifySelf: 'flex-start' }}>
+        <SimpleFlexUnit style={{ whiteSpace: 'nowrap' }}>
+          {info.data.code}
+        </SimpleFlexUnit>
+        <SimpleFlexUnit>
           <OnetLink socCode={info.data.code} occupation={info.data.name}>
             {info.data.name}
           </OnetLink>
-        </div>
+        </SimpleFlexUnit>
+        <SimpleFlexUnit>{info.data.category}</SimpleFlexUnit>
       </SimpleFlexRow>
-      <SimpleFlexRow>
-        <div style={{ flex: 3 }}>{info.data.category}</div>
+      <SimpleFlexRow style={{ justifyContent: 'flex-end' }}>
+        <SimpleFlexUnit>
+          {`Transition Share: ${
+            Math.round(info.data.transitionRate * 10000) / 100
+          }%`}
+        </SimpleFlexUnit>
+        <SimpleFlexUnit>Salary: </SimpleFlexUnit>
+        <SimpleFlexRow style={{ display: 'flex', flexDirection: 'row' }}>
+          <SimpleFlexUnit>
+            Hourly {`$${hourlyTwoDecimal(info.data.hourlyPay)}`}
+          </SimpleFlexUnit>
+          <SimpleFlexUnit>
+            Annual {`$${annualFormatted(info.data.annualSalary)}`}
+          </SimpleFlexUnit>
+        </SimpleFlexRow>
       </SimpleFlexRow>
-      <div
-        style={{
-          display: 'flex',
-          flex: 2,
-          flexDirection: 'row',
-          marginRight: '.5em',
-        }}
-      >
-        <div style={{ flex: 1, fontVariantNumeric: 'lining-nums' }}>
-          Transition Share:
-          {Math.round(info.data.transitionRate * 10000) / 100}%
-        </div>
-        <div
-          style={{
-            display: 'flex',
-            flex: 2,
-            flexDirection: 'row',
-            fontVariantNumeric: 'tabular-nums lining-nums',
-            textAlign: 'right',
-          }}
-        >
-          Salary:&nbsp; Hourly ${`${hourlyTwoDecimal(info.data.hourlyPay)}`}
-          &nbsp; Annual ${`${annualFormatted(info.data.annualSalary)}`}&nbsp;
-        </div>
-      </div>
-      <hr></hr>
     </TextContainer>
   );
 };
